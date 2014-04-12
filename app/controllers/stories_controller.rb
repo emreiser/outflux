6ff0248d5106131a7e92d36e5ba9cad709a31c11
emreiser <emilie.reiser@gmail.com>
2014-04-11 21:13:02 -0400

    Reduced expiry time for story cache and sorted stories decending by date.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,12 +4,12 @@ class StoriesController < ApplicationController
     @country = Country.find_by(code: params[:code])
 
     if @country.emergency
-      Rails.cache.fetch("cached_stories_#{@country.id}", expires_in: 2.days) do
+      Rails.cache.fetch("cached_stories_#{@country.id}", expires_in: 1.days) do
         @country.getStories
       end
     end
 
-    @stories = @country.stories.limit(4)
+    @stories = @country.stories.sort_by(&:created_at).reverse
 
     render json: @stories, meta: @country
   end

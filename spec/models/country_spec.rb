35cb42262b21eaf3e38b0d614cadd1d5c48d7c85
emreiser <emilie.reiser@gmail.com>
2014-05-10 13:43:23 -0400

    Fixed duplicate story bug in stories controller. Modified tests.

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -7,20 +7,18 @@ describe Country do
   end
 
   describe "relationships" do
-    it {should have_many :origins }
-    it {should have_many :destinations }
+    it {should have_many :origin_counts }
+    it {should have_many :destination_counts }
   end
 
   describe "#getPage" do
-
-    it "should return an array of Nokogiri elements if found" do
+    it "should return an array of Nokogiri elements" do
       expect(@country.getPage.class).to be Nokogiri::XML::NodeSet
     end
   end
 
   describe "#getStories" do
-
-    it "should create story instances asssociated with the country if found" do
+    it "should create story instances asssociated with the country" do
       @country.getStories
       expect(@country.stories.count).to_not eq 0
     end
